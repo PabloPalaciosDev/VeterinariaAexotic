748af8d810de2362dbc60d3ee1865a15b20645cf
Alik Dev <moisesalik01@gmail.com>
2022-11-27 12:08:45 -0500

    fix #2, loadLogic's agregados

---
+++ frontend/js/app.js
@@ -0,0 +1,37 @@
+const loadHome = () => {
+    console.log('Home funcionando');
+}
+
+const loadDescrip = () => {
+    document.getElementById('boton').addEventListener('click', e => {
+        document.getElementById('box').textContent = 'FUNCIONAAAAA???!!!!! !NO SE'
+    })
+}
+
+const loadServicios = () => {
+    console.log('Servicios funcionando');
+}
+
+
+const loadContactanos = () => {
+    console.log('Contactos funcionando');
+}
+
+
+const loadLogin = () => {
+    console.log('Login funcionando');
+}
+
+
+const loadRegister = () => {
+    console.log('Register funcionando');
+}
+
+
+const loadBusqueda = () => {
+    console.log('Busqueda funcionando');
+}
+
+const loadError = () => {
+    console.log('Error funcionando');
+}
--- frontend/js/router.js
+++ frontend/js/router.js
@@ -4,11 +4,6 @@
  */
 
 
-//AQUI SE VA A CARGAR TODA LA LÓGICA DE LA APLICACIÓN, DOM SCRIPTING, CONSULTAS FETCH, ETC.
-function cargarLogicaApp () {
-    
-}
-
 //Función para parametrizar la carga de las plantillas HTML
 const imprimirPlantillaGeneralFunction = async (path, title, currentLink = undefined) => {
     let plantilla = await fetch('/frontend/html/header.html').then(data => data.text());
@@ -17,10 +12,6 @@ const imprimirPlantillaGeneralFunction = async (path, title, currentLink = undef
     document.getElementById('root').innerHTML = plantilla;
     window.document.title = title
     document.getElementById('header-menu').children[currentLink]?.classList.add('current-link');
-    /* if(currentLink === 4)
-        document.getElementById('link-login').remove();
-    if(currentLink === 5)
-        document.getElementById('link-register').remove(); */
 }
 
 //Objeto mapa con las diferentes rutas
@@ -29,7 +20,8 @@ const rutasPlantillas = {
         title: 'Home',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/index.html', this.title, 0);
-        }
+        },
+        loadLogic: loadHome,
     },
     /* '/about': {
         title: 'About',
@@ -41,37 +33,50 @@ const rutasPlantillas = {
         title: 'Servicios',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/servicios.html', this.title, 2);
-        }
+        },
+        loadLogic: loadServicios,
     },
     '/contactanos': {
         title: 'Contactanos',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/Contactos.html', this.title, 3);
-        }
+        },
+        loadLogic: loadContactanos,
     },
     '/login': {
         title: 'Login',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/login.html', this.title, 4);
-        }
+        },
+        loadLogic: loadLogin,
     },
     '/register': {
         title: 'Register',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/registro.html', this.title, 5);
-        }
+        },
+        loadLogic: loadRegister,
     },
     '/busqueda': {
         title: 'Busqueda',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/busqueda.html', this.title);
-        }
+        },
+        loadLogic: loadBusqueda,
+    },
+    '/descripcion': {
+        title: 'Descripción',
+        imprimirPlantilla: async function () {
+            await imprimirPlantillaGeneralFunction('/frontend/html/descripcion.html', this.title);
+        },
+        loadLogic: loadDescrip
     },
     error: {
         title: 'Error',
         imprimirPlantilla: async function () {
             await imprimirPlantillaGeneralFunction('/frontend/html/404.html', this.title);
-        }
+        },
+        loadLogic: loadError,
     }
 }
 
@@ -80,7 +85,7 @@ const cargarRutaRequest = async (ruta) => {
     const rutaPlantilla = rutasPlantillas[ruta] || rutasPlantillas.error;
     window.history.pushState({}, 'done', ruta);
     await rutaPlantilla.imprimirPlantilla();
-    cargarLogicaApp();
+    rutaPlantilla.loadLogic();
 };
 
 //Esta función inicia el router, se puede decir que es como el MAIN de nuestra app
@@ -92,4 +97,3 @@ const rutaRequest = () => {
 };
 
 rutaRequest();
-

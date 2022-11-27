8d844ab972d6485ea0dd34642aae4e29538faff7
Pablo Palacios <ppalacios500@gmail.com>
2022-11-27 09:30:25 -0500

    nada

--- backend/src/main/java/com/example/veterinarianelo/Services/Conexion.java
+++ backend/src/main/java/com/example/veterinarianelo/Services/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
             String connectionUrl = "jdbc:sqlserver://localhost:1433;"
             + "databaseName=VeterinariaNelo;"
             + "user=sa;"
-            + "password=VUELVE A COLOCAR TU PASSWORD;"
+            + "password=12345678;"
             + "trustServerCertificate=true";
             Connection con = DriverManager.getConnection(connectionUrl);
             return con;
